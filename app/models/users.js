f77133108c8e45b2bf899560dd9d59cb4d34f4d1
Drew Sartorius <duckydesign@gmail.com>
2016-03-29 13:36:07 -0400

    authentication working for FB,T,G

---
+++ app/models/users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+    local: {
+        email: String,
+        password: String
+    },
+    google: {
+        id: String,
+        token: String,
+        email: String,
+        name: String
+    },
+    twitter: {
+        id: String,
+        displayName: String,
+        token: String,
+        username: String
+    },
+    facebook: {
+        id: String,
+        token: String,
+        email: String,
+        name: String
+    }
+});
+
+module.exports = mongoose.model('User', User);
--- server.js
+++ server.js
@@ -5,40 +5,40 @@ var app = express();
 require('dotenv').load();
 
 var routes = require("./app/routes/index.js");
-// var mongoose = require('mongoose');
-// var passport = require('passport');
-// var session = require('express-session');
-// var parser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+var parser = require('body-parser');
 
 var http = require('http').Server(app);
 
-// require("./app/config/passport.js")(passport);
+require("./app/config/passport.js")(passport);
 
-// var mongooseUrl = process.environment.MLAB_URI;
-// mongoose.connect(mongooseUrl);
+var mongooseUrl = process.env.MLAB_URI;
+mongoose.connect(mongooseUrl);
 
 app.use("/client", express.static(process.cwd()+"/client"));
 // app.use("/controllers", express.static(process.cwd()+"/app/controllers"));
 // app.use("/common", express.static(process.cwd()+"/app/common"));
-// app.use("/views", express.static(process.cwd()+"/app/views"));
+app.use("/views", express.static(process.cwd()+"/app/views"));
 
-// app.use(parser.json());
-// app.use(parser.urlencoded({extended:true}));
+app.use(parser.json());
+app.use(parser.urlencoded({extended:true}));
 
-// app.set('views',process.cwd()+'/app/views');
-// app.set('view engine','jade');
+app.set('views',process.cwd()+'/app/views');
+app.set('view engine','jade');
 
-// app.use(session({
-    // secret: 'shhhhhh',
-    // resave: false,
-    // saveUninitialized: true
-// }));
+app.use(session({
+    secret: 'shhhhhhh',
+    resave: false,
+    saveUninitialized: true
+}));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-routes(app);
-// routes(app,passport);
+// routes(app);
+routes(app,passport);
 
 // app.use(function(req,res,next){
     // res.status(404);
